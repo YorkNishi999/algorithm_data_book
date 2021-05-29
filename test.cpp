428a08bc4e34ccd252dade3254ece4431d24fa68
YoheiNishimura <york.nishi@gmail.com>
2021-05-30 00:51:58 +0900

    add new prob

--- test.cpp
+++ test.cpp
@@ -1,40 +1,95 @@
-#include<iostream>
 #include<cstdio>
 #include<cstdlib>
 #include<cstring>
+
 using namespace std;
-int top,s[1000];
-int pop(){
-	top--;
-	return s[top+1];
+
+struct Node {
+    int key;
+    Node *next, *prev;
+};
+
+Node *nil;
+
+void init(){
+    nil = (Node *)malloc(sizeof(Node));
+    nil->prev = nil;
+    nil->next = nil;
 }
-void push(int a){
-	s[++top]=a;
+
+Node* listSearch(int key){
+    Node* cur = nil->next;
+    while(cur != nil && cur->key != key){
+        cur = cur->next;
+    }
+    return cur;
 }
+
+void printList(){
+    Node* cur = nil->next;
+    int isFirst = 0;
+    while(1){
+        if(cur == nil) break;
+        if(isFirst++ > 0)   printf(" ");
+        printf("%d" , cur->key);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+void insert(int key){
+    Node* x = (Node *)malloc(sizeof(Node));
+    x->key = key;
+    
+    x->next = nil->next;
+    nil->next->prev = x;
+    nil->next = x;
+    x->prev = nil;
+}
+
+void deleteNode(Node* del){
+    if(del == nil) return;
+    del->prev->next = del->next;
+    del->next->prev = del->prev;
+    free(del);
+}
+
+void deleteFirst(){
+    deleteNode(nil->next);
+}
+
+void deleteLast(){
+    deleteNode(nil->prev);
+}
+
+void deleteKey(int key){
+    deleteNode(listSearch(key));
+}
+
 int main(){
-	int b,a;
-	top=0;
-	char c[3];
-	while(cin>>c){
-		if(c[0]=='+'){
-			a=pop();
-			b=pop();
-			push(a+b);
-		}
-		else if(c[0]=='-'){
-			a=pop();
-			b=pop();
-			push(b-a);
-		}
-		else if(c[0]=='*'){
-			a=pop();
-			b=pop();
-			push(a*b);
-		}
-		else{
-			push(atoi(c));
-		}
-	}
-	cout<<pop()<<endl;
-	return 0;
+    char op[20];
+    int key, n, i;
+
+    scanf("%d", &n);
+    init();
+    for(i = 0; i < n; i++){
+        scanf("%s%d" , op , &key);
+        if(op[0] == 'i'){
+            insert(key);
+        }else if(op[0] == 'd'){
+            if(strlen(op) > 6){
+                if(op[6] == 'F'){
+                    deleteFirst();
+                }else if(op[6] == 'L'){
+                    deleteLast();
+                }
+            }else{
+                deleteKey(key);
+            }
+        }
+    }
+
+    printList();
+
+    return 0;
 }
